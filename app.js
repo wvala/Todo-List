acf7faeaa9799470dae6db7c74bab9532f3437c8
wvala <will.valentine1603@gmail.com>
2022-08-09 16:59:39 +0100

    working to-do list without storage

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ handleLogoClick = () => {
   const url = "https://animechan.vercel.app/api/random";
 
   display = (quote) => {
-    alert(`${quote.quote} - ${quote.character}`);
+    alert(`"${quote.quote}" - ${quote.character}`);
   }
 
   fetch(url)
@@ -14,11 +14,55 @@ document.querySelector('form').addEventListener('submit', handleSubmitForm);
 
 function handleSubmitForm(e) {
   e.preventDefault();
-  let input = document.querySelector('input')
+  let input = document.querySelector('input');
   if (input.value != '') {
     addTodo(input.value);
   } else {
-    alert("Please write something first!");
+    alert("Please write a task first!");
   }
+
   input.value = '';
+}
+
+function addTodo(todo) {
+  let ul = document.querySelector('ul');
+  let li = document.createElement('li');
+  li.innerHTML = `
+      <span class="todo-item">${todo}</span>
+      <button name="checkButton"><i class="fa-solid fa-check"></i></button>
+      <button name="deleteButton"><i class="fa-solid fa-trash"></i></button>
+  `;
+  li.classList.add('todo-list-item');
+  ul.appendChild(li);
+}
+
+document.querySelector('ul').addEventListener('click', handleClickDeleteOrCheck);
+
+function handleClickDeleteOrCheck(e) {
+  if (e.target.name == 'checkButton') {
+    checkTodo(e);
+  } else if (e.target.name == 'deleteButton') {
+    deleteTodo(e);
+  }
+}
+
+function checkTodo(e) {
+  let item = e.target.parentNode;
+  if (item.style.textDecoration == 'line-through') {
+    item.style.textDecoration = 'none';
+  } else {
+    item.style.textDecoration = 'line-through';
+  }
+}
+
+function deleteTodo(e) {
+  let item = e.target.parentNode;
+
+  item.remove();
+}
+
+document.getElementById('clearAll').addEventListener('click', handleClearAll);
+
+function handleClearAll(e) {
+  document.querySelector('ul').innerHTML = '';
 }
